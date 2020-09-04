ae0a30ab51df46f88ac8de7164cc5a184c40ac0a
Gustaf <gustaf.stralin@skola.botkyrka.se>
2020-09-04 15:06:13 +0200

    light sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
 
 void setup() {
+  Serial.begin(9600);
+  pinMode(A5, OUTPUT);
   // put your setup code here, to run once:
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  int value = analogRead(A0);
+  if (value < 970) {
+    digitalWrite(A5, HIGH);
+  }
+  else if (value > 970) {
+    digitalWrite(A5, LOW);
+  }
+  delay(10);
+
 }
